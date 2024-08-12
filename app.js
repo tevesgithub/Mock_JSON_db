d02d3f8956b54ec404c5a400a4bd93c86e79046f
tevesgithub <vjteves@gmail.com>
2024-08-12 11:40:24 -0600

    mock_json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const userRoute = require('./routes/users.js');
 require('dotenv').config();
 
 
@@ -9,10 +10,11 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static('public'));
+app.use(userRoute);
 
 
 app.listen(PORT,()  => {
-    console(`Connected on port ${PORT}`);
+    console.log(`Connected on port ${PORT}`);
 
 
 
